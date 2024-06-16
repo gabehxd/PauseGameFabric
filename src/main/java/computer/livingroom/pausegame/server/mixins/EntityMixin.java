304c5d586d7ab318ef8518b043d2a334e284ea23
gabeh <gabehttps@gmail.com>
2024-06-15 21:28:33 -0700

    wrap is better

--- src/main/java/computer/livingroom/pausegame/server/mixins/EntityMixin.java
+++ src/main/java/computer/livingroom/pausegame/server/mixins/EntityMixin.java
@@ -1,26 +1,25 @@
 package computer.livingroom.pausegame.server.mixins;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import computer.livingroom.pausegame.server.PauseGameServer;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
     @Shadow
     public abstract MinecraftServer getServer();
 
-    @Inject(
+    @WrapWithCondition(
             method = "setAirSupply",
-            at = @At("HEAD"),
-            cancellable = true
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/network/syncher/SynchedEntityData;set(Lnet/minecraft/network/syncher/EntityDataAccessor;Ljava/lang/Object;)V")
     )
-    private void doNotUpdateAirSupply(int air, CallbackInfo ci) {
-        if (this.getServer().tickRateManager().isFrozen() && PauseGameServer.settings.enableModSupport())
-            ci.cancel();
+    private boolean doNotUpdateAirSupply(SynchedEntityData instance, EntityDataAccessor<Object> key, Object value) {
+        return this.getServer().tickRateManager().isFrozen() && PauseGameServer.settings.enableModSupport();
     }
 }
